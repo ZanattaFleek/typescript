547e260035583e1dbd545fb777862fa01399b1bc
zanattafleek <zanattafleek@gmail.com>
2022-08-18 20:19:31 -0300

    LeituraArquivo

--- exercicioArquivo/src/index.ts
+++ exercicioArquivo/src/index.ts
@@ -1,1 +1,16 @@
-console.log('oi...')
+/*
+
+1 - Leitura do Arquivo e incluir no Array
+
+
+*/
+
+import ClsArquivo from "./clsArquivo";
+
+let clsArquivo: ClsArquivo = new ClsArquivo()
+
+clsArquivo.lerArquivo('alunos2.txt').then(() => {
+    console.log('Conseguir Calcular... Vou imprimir....')
+}).catch(err => {
+    console.log(err)
+})
