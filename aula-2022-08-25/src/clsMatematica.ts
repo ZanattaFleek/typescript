53a9f525a1e4a20eb81fbaede09a06905af1ec7e
zanattafleek <zanattafleek@gmail.com>
2022-08-25 19:36:00 -0300

    Sincrono

---
+++ aula-2022-08-25/src/clsMatematica.ts
@@ -0,0 +1,9 @@
+/**
+ * Realiza Operações Matemáticas - Básicas
+ */
+export default class ClsMatematica {
+    public somar(numero01: number, numero02: number) {
+        console.log('02 - Dentro do Código')
+        return numero01 + numero02
+    }
+}
---
+++ aula-2022-08-25/src/index.ts
@@ -0,0 +1,9 @@
+import ClsMatematica from "./clsMatematica";
+
+let clsMat: ClsMatematica = new ClsMatematica()
+
+console.log('01 - Início do Código')
+
+console.log('03', clsMat.somar(10, 20))
+
+console.log('04 - Término do Código')
